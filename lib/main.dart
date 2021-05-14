ea7cca2cd5277ea220f0e433d44edc57f75aacdb
FlutLab.io <>
2021-05-14 10:02:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,15 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
+
+  void _pushSaved() {}
 
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
